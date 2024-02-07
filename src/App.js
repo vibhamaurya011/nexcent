d909ff5f01c409a45a5a898a8c3516175cc72560
vibhamaurya011 <vibhamaurya011@gmail.com>
2024-02-07 15:51:32 +0530

    done

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import HomePage from './components/HomePage';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <>
+    <Header/>
+    <HomePage/>
+    <Footer/>
+    </>
+  );
+}
+
+export default App;
